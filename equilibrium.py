ed665ec12c6d6cda4751b79107b40f4e28c4a671
Chanathip Srithanrat <omongo@gmail.com>
2014-07-03 19:30:04 +0700

    Fix a minor bug

--- equilibrium.py
+++ equilibrium.py
@@ -1,10 +1,11 @@
 def solution(A):
     left_sum = 0
     right_sum = sum(A)
-    for i in range(1, len(A) - 1):
-        left_sum += A[i - 1]
-        if left_sum == right_sum - A[i] - left_sum:
+    for i in range(len(A)):
+        right_sum -= A[i]
+        if left_sum == right_sum:
             return i
+        left_sum += A[i]
     return -1
 
 A = [-7, 1, 5, 2, -4, 3, 0]
