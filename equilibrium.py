6fb275dafedc11b23b9589dab32a55faad85950a
Chanathip Srithanrat <omongo@gmail.com>
2014-07-03 15:37:43 +0700

    Initial commit

---
+++ equilibrium.py
@@ -0,0 +1,11 @@
+def solution(A):
+    left_sum = 0
+    right_sum = sum(A)
+    for i in range(1, len(A)):
+        left_sum += A[i - 1]
+        if left_sum == right_sum - A[i] - left_sum:
+            return i
+    return -1
+
+A = [-7, 1, 5, 2, -4, 3, 0]
+print(solution(A))
