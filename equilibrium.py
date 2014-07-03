994ce6d4ed4c881dce62588bb229c3923dbb0ffb
Chanathip Srithanrat <omongo@gmail.com>
2014-07-03 18:05:46 +0700

    Fix off-by-one error

--- equilibrium.py
+++ equilibrium.py
@@ -1,7 +1,7 @@
 def solution(A):
     left_sum = 0
     right_sum = sum(A)
-    for i in range(1, len(A)):
+    for i in range(1, len(A) - 1):
         left_sum += A[i - 1]
         if left_sum == right_sum - A[i] - left_sum:
             return i
